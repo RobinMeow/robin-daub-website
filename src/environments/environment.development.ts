cfff4e36c2d26aa68bf2b67829960bd8aa95597c
Robin <robinmeow97@gmail.com>
2024-10-05 19:01:54 +0200

    update readme with how to get firebase configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,10 @@ export const environment = {
   useEmulatorFirestore: true,
   firebase: {
     projectId: 'demo-project',
+    apiKey: undefined,
+    authDomain: undefined,
+    storageBucket: undefined,
+    messagingSenderId: undefined,
+    appId: undefined,
   },
 };
